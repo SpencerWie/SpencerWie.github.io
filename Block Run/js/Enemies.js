db18dde91912a4d0182a0f345155df8c748fcaa0
Spencer Wieczorek <spencerwie@gmail.com>
2024-11-19 19:37:20 -0500

    Formatting

--- Block Run/js/Enemies.js
+++ Block Run/js/Enemies.js
@@ -1,31 +1,30 @@
 function Enemy(x, y, width, height, image, speed ,walkSteps, hp, type) 
 {
-   this.x = x;
-   this.y = y;
-   this.frameX = 0; // X frame on tilemap sprite
-   this.frameY = 0; // Y frame on tilemap sprite
-   this.width = width;
-   this.height = height;
-   this.sWidth = width; this.sHeight = height;
-   this.image = image;
-   this.speed = speed;
-   this.startWalk = this.x;
-   this.endWalk = this.x + (walkSteps * 32) - (this.width - 32);
-   this.hp = hp;
-   this.type = type
+	this.x = x;
+	this.y = y;
+	this.frameX = 0; // X frame on tilemap sprite
+	this.frameY = 0; // Y frame on tilemap sprite
+	this.width = width;
+	this.height = height;
+	this.sWidth = width; this.sHeight = height;
+	this.image = image;
+	this.speed = speed;
+	this.startWalk = this.x;
+	this.endWalk = this.x + (walkSteps * 32) - (this.width - 32);
+	this.hp = hp;
+	this.type = type
 	
 	if(type == "red block") this.y -= 20;
 	if(type == "spike") { this.sWidth = 35; this.sHeight = 35; }
    
-   this.draw = function(){
-      this.update();
-      ctx.drawImage(this.image, this.frameX*this.sWidth, this.frameY*this.sHeight, this.sWidth, this.sHeight, this.x, this.y, this.sWidth, this.sHeight);
-   }
+	this.draw = function(){
+    	this.update();
+    	ctx.drawImage(this.image, this.frameX*this.sWidth, this.frameY*this.sHeight, this.sWidth, this.sHeight, this.x, this.y, this.sWidth, this.sHeight);
+   	}
    
    this.update = function() 
    {
-      if(DEAD) return;
-      // Walking
+    	if(DEAD) return;
 		if(type == "red block")
 		{
 			if(this.x >= this.endWalk && this.speed > 0){
