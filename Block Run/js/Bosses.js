febce72df687ececa749092d05c223d2dee904f2
Spencer Wieczorek <spencerwie@gmail.com>
2025-03-14 23:29:13 -0400

    BigJelly class structure

--- Block Run/js/Bosses.js
+++ Block Run/js/Bosses.js
@@ -8,6 +8,139 @@ function BigRedAttack(x, y)
 
 }
 
+function BigJelly(x, y, index) 
+{
+	this.x = x;
+	this.y = y;
+	this.index = index;
+	this.frameX = 0; // X frame on tilemap sprite
+	this.frameY = 0; // Y frame on tilemap sprite
+	this.width = 128;
+	this.height = 128;
+	this.image = images['BigJelly'];
+	this.atk = new BigRedAttack(this.x, this.y);
+	this.speed = -4;
+	this.stop = false;
+	this.hit = false;
+	this.hp = 10; // If on Level 2 this is the diamond unlock BigJelly so more hp.
+	this.maxHp = this.hp;
+	this.attackDelay = 0;     // Timer to wait before attacking
+	this.attackTimer = 0;     // Timer it takes to do an attack
+	this.vulnerableTimer = 0; // Timer for how long boss is exposed to damage
+	this.deathTimer = 0;      // On death how long before boss is removed for death animation.
+	this.energy = 2;
+
+	this.draw = function() {
+		this.update();
+
+		// When vulnerable add transparency to draw
+		var vulnerable = this.hit || this.deathTimer > 0
+		if(this.isDarkRed) ctx.globalCompositeOperation="multiply"; // Challenge BigJelly is darker in color
+		if(vulnerable) ctx.globalAlpha = 0.5;
+		ctx.drawImage(this.image, this.frameX*this.width, this.frameY*this.height, this.width, this.height - this.deathTimer, this.x, this.y + this.deathTimer, this.width, this.height - this.deathTimer);
+		if(vulnerable) ctx.globalAlpha = 1;
+
+
+		if(this.attackTimer) {
+			// Do Big Jelly attacks. The boss goes between a few seperate attaks. A whole area attack that hits the entire map and homing ligning strikes from above.
+		}
+		
+	}
+
+	this.update = function() {
+
+		var foundBoss = Enemies.find(item => isItem(item, "BigJelly"));
+		// On death remove from item list
+		if(this.hp <= 0) {
+			if(this.deathTimer > 128) {
+				if(foundBoss) {
+					KEYS++;
+					COINS+=200;
+					if(!beatBigJelly()) COINS+=1000;
+					Enemies = Enemies.filter(item => !isItem(item, "BigJelly"));
+					boss = false;
+					delete this;
+				}
+			}
+			this.deathTimer++;
+			this.frameX = 3;
+			return;
+		}
+
+		this.handleMovementAndCollisions();
+		this.handleAttacks();
+	}
+
+	this.handleMovementAndCollisions = function() {
+		// The lower the boss hp the faster it moves
+		if(this.speed > 0) this.speed = 3 + ((this.maxHp - this.hp)/2);
+		else this.speed = -3 - ((this.maxHp - this.hp)/2);
+
+		var blocks = getBlocksNearItem(this, 5);
+		for(var Y = blocks.above; Y < blocks.below; Y++ ) {
+			 for(var X = blocks.left; X < blocks.right; X++ ) {
+				var item = MapItems[Y][X];
+				if(!item) continue;
+				var isSolidBlock = (isItem(item,'block') || isItem(item,'lock'));  
+				if (isSolidBlock && collide(item, this)) {
+					if(this.speed > 0) 
+						this.x = item.x - this.width - 1; // Turn around and be a pixel away from the wall to the right
+					else 
+						this.x = item.x + 33; // Turn around and be a pixel away from the block to the left
+					this.speed *= -1;
+					break;
+				}
+			}
+		}
+		if(!this.stop) this.x += this.speed;
+	}
+
+	this.handleAttacks = function() {
+		// If we are active to attack wait until the delay ends and then trigger attack animation
+		if(this.attackDelay > 0) {
+			this.attackDelay--;
+			if(this.attackDelay <= 0) this.startAttack();
+		}
+
+		// If we are currently attacking draw attack animation
+		if(this.attackTimer > 0) {
+			this.attackTimer--;
+			// If close to death charge up part of the attack is twice as fast
+			if(this.hp <= 2 && this.attackTimer > 70) this.attackTimer--; 
+			if(this.attackTimer <= 0) {
+				this.stop = false;
+				this.frameX = 0;
+				this.energy--;
+				if(this.energy == 0) this.vulnerableTimer = 100 + (this.hp*5); // Invulnerablity last slightly less as hp lowers
+			}
+		}
+
+		// If out of energy after doing an attack boss is vulnerable to taking damage
+		if(this.vulnerableTimer > 0) {
+			this.vulnerableTimer--;
+			this.stop = true;
+			this.frameX = this.hit ? 3 : 2;
+			if(this.vulnerableTimer == 0) {
+				this.stop = this.hit = false;
+				this.frameX = 0;
+				this.energy =  1 + Math.ceil(Math.random() * 2);
+			}
+		}
+	}
+
+	this.startAttack = function() {
+		this.frameX = 1;
+		this.stop = true;
+		this.attackTimer = 110;
+	}
+
+	this.takeDamage = function() {
+		this.hp--;
+		this.vulnerableTimer = 200;
+		this.hit = true;
+	}
+}
+
 function BigRed(x, y, index) 
 {
 	this.x = x;
